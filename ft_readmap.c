19cc2bee9d81a91661d51712a2b816390da4ffe6
Chayma El hasnaoui <chael-ha@e1r10p14.1337.ma>
2021-09-24 19:19:13 +0100

    Second

--- ft_readmap.c
+++ ft_readmap.c
@@ -6,7 +6,7 @@
 /*   By: chael-ha <chael-ha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 14:25:00 by chael-ha          #+#    #+#             */
-/*   Updated: 2021/09/21 18:13:21 by chael-ha         ###   ########.fr       */
+/*   Updated: 2021/09/24 19:00:26 by chael-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -185,8 +185,12 @@ char	**ft_readmap(char *filename, t_mlx *mlx)
 	check_map(mlx->lines, mlx);
 	mlx->max_x = countx(mlx->lines);
 	mlx->max_y = county(mlx->lines);
+	WINDOW_HEIGHT = mlx->max_x * SIZE;
+	WINDOW_WIDTH = mlx->max_y * SIZE;
+	// WINDOW_HEIGHT = 1024;
+	// WINDOW_WIDTH = 1024;
 	is_map_rectang(mlx);
-	//printf("m here\n");
+	printf("width = %d and height = %d are\n", WINDOW_WIDTH, WINDOW_HEIGHT);
 	is_map_closed(mlx);
 	is_map_valid(mlx);
 	return (mlx->lines);
@@ -214,15 +218,15 @@ char	**ft_readmap(char *filename, t_mlx *mlx)
 // 	}
 // }
 
-#include <mlx.h>
+// #include <mlx.h>
 
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
+// typedef struct	s_data {
+// 	void	*img;
+// 	char	*addr;
+// 	int		bits_per_pixel;
+// 	int		line_length;
+// 	int		endian;
+// }				t_data;
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
@@ -232,10 +236,67 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-void    draw_square(t_data *data, t_mlx *mlx)
+int	ft_load_texture(char *path, t_data *data)
+{
+	t_mlx	mlx;
+
+	mlx.win.mlx_ptr = mlx_init();
+	data->img = mlx_xpm_file_to_image(mlx.win.mlx_ptr, path, &data->img_width, &data->img_height);
+	// if (!mlx.mlx_img.img_ptr)
+	// 	ft_err_print("error in texture path: ", path);
+	data->img = (int *)mlx_get_data_addr(data->img,
+			&data->bits_per_pixel, &data->line_length, &data->endian);
+	return (1);
+}
+
+void map_texture_array(void)
+{
+	float ratio_x = (float)texture.img_width/(float)SIZE; 
+	float ratio_y = (float)texture.img_height/(float)SIZE; 
+	mapped_texture.img = mlx_new_image(mlx_init(), SIZE, SIZE);
+	mapped_texture.addr = mlx_get_data_addr(mapped_texture.img, &mapped_texture.bits_per_pixel, &mapped_texture.line_length,&mapped_texture.endian);
+	int i = -1;
+	// float ii = 0;
+	while(++i < SIZE)
+	{
+		int j = -1;
+		// float jj = 0;
+		while(++j < SIZE)
+		{
+			my_mlx_pixel_put(&mapped_texture, i, j, ((int *)texture.img)[(int)(j * ratio_y) * texture.img_width + (int)(i * ratio_x)]);
+			// j++;
+			// jj += ratio_y;
+		}
+		// ii += ratio_x;
+	}
+}
+
+void	draw_square(t_data *data,int i, int j, t_mlx mlx)
+{
+	// int x;
+	// int	y;
+
+	// x = 0;
+	// while (x < SIZE)
+	// {
+	// 	y = 0;
+	// 	while (y < SIZE)
+	// 	{
+			//and here
+
+			mlx_put_image_to_window(mlx.win.mlx_ptr, mlx.win.win_ptr, mapped_texture.img, j * SIZE,i * SIZE);
+			//my_mlx_pixel_put(data, x + (j * SIZE), y + (i * SIZE), 0x00ff00ff);
+	// 		y++;
+	// 	}
+	// 	x++;
+	// }
+}
+
+void    draw_map(t_data *data, t_mlx *mlx)
 {
     int i;
     int j;
+
 
     i = 0;
     while (mlx->lines[i])
@@ -243,8 +304,8 @@ void    draw_square(t_data *data, t_mlx *mlx)
         j = 0;
         while (mlx->lines[i][j])
         {
-			if (mlx->lines[i]]j] == '1')
-            	my_mlx_pixel_put(data, j * SIZE, i * SIZE, 0x00ff00ff);
+			if (mlx->lines[i][j] == '1')
+				draw_square(data, i, j, *mlx);//here
 			j++;
         }
         i++;
@@ -258,7 +319,7 @@ int main(int argc, char **argv)
 	t_mlx	mlx;
 	t_data	data;
 	char **s;
-
+	data.relative_path = "/Users/chael-ha/Desktop/ft_cub 2/textures/bit.xpm";
 	s = ft_readmap(argv[1], &mlx);
 	i = 0;
 	while(s[i])
@@ -266,7 +327,6 @@ int main(int argc, char **argv)
 		printf("%s| %d\n",s[i], i);
 		i++;
 	}
-
 	mlx.win.mlx_ptr = mlx_init();
 	mlx.win.win_ptr = mlx_new_window(mlx.win.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "./so_long");
 	data.img = mlx_new_image(mlx.win.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -278,8 +338,12 @@ int main(int argc, char **argv)
 	** then be set accordingly for the *current* data address.
 	*/
 	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.line_length,&data.endian);
+	// t_mlx	mlx2;
+	// mlx2.win.mlx_ptr = mlx_init();
+	ft_load_texture("/Users/chael-ha/Desktop/ft_cub 2/textures/bit.xpm", &texture);
+	map_texture_array();
 	//my_mlx_pixel_put(&data, 5, 5, 0x0000FF00);
-	draw_square(&data, &mlx);
-	mlx_put_image_to_window(mlx.win.mlx_ptr, mlx.win.win_ptr, data.img, 0, 0);
-	mlx_loop(mlx.win.mlx_ptr);
+	draw_map(&data, &mlx);
+	//mlx_put_image_to_window(mlx.win.mlx_ptr, mlx.win.win_ptr, data.img, 0, 0);
+	mlx_loop(mlx.win.mlx_ptr); //window rendering
 }
--- structs.h
+++ structs.h
@@ -6,18 +6,36 @@
 /*   By: chael-ha <chael-ha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 14:25:09 by chael-ha          #+#    #+#             */
-/*   Updated: 2021/09/21 18:14:19 by chael-ha         ###   ########.fr       */
+/*   Updated: 2021/09/24 18:53:50 by chael-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <mlx.h>
 
 #define FALSE	0
 #define TRUE	1
-#define	WINDOW_WIDTH	
-#define	WINDOW_HEIGHT	1080
-#define SIZE 42
+#define SIZE	30
 
+
+int	WINDOW_HEIGHT = FALSE;
+int	WINDOW_WIDTH  = FALSE;
 int isGameRunning = FALSE;
+
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+	char	*relative_path;
+	int		img_width;
+	int		img_height;
+}				t_data;
+
+
+t_data mapped_texture;
+t_data texture;
 
 typedef struct		s_window
 {
